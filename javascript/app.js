5ed40c29804503492f20119ba3d330527fec9730
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-01-29 12:22:11 +0100

    V1.0

---
+++ javascript/app.js
@@ -0,0 +1,23 @@
+class Car {
+    constructor(name, color, speed, price) {
+        this.name = name;
+        this.color = color;
+        this.speed = speed;
+        this.price = price;
+    }
+    add() {
+        
+    }
+    remove() {
+        
+    }
+    update(){
+
+    }
+    display(){
+
+    }
+}
+
+let car = new Car("BMW", "Black", 220, 2000000)
+console.log(car);
